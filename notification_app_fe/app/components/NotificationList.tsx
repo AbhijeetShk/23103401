83b95dbe292e9ca59f1185b9a4fb52263d6c56b5
Abhijeet <programmerdostofficial@gmail.com>
2026-05-08 16:43:52 +0530

    fix: imports updated for notificationList

--- notification_app_fe/app/components/NotificationList.tsx
+++ notification_app_fe/app/components/NotificationList.tsx
@@ -1,5 +1,7 @@
 "use client";
+import { useEffect, useState } from "react";
 import NotificationCard from "./NotificationCard";
+import { fetchNotifications } from "../services/notification";
 
 type Props = {
   type: string;
